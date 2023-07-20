6a8370280c11368cc3d9e2f6998fc4d55bd17481
kgolden4514 <kmadams6@ncsu.edu>
2023-07-20 02:29:56 -0400

    Trying to get graph to render

--- project3/server.R
+++ project3/server.R
@@ -5,6 +5,17 @@ library(tidyverse)
 
 shinyServer(function(input, output, session) {
   setwd("C:/Documents/Github/project3")
-  nhl <- read.csv('hockeydata.csv')
-  nhl <- nhl[ , -c(51:102)]
+  student <- read.csv('data.csv')
+  
+  # Create Plot
+  output$height <- renderPlot({
+    #get filtered data
+    if (input$grade == '9th grade'){
+    th9 <- student %>% filter(Grade == 9)
+    g <- ggplot(th9, aes(x = Height_in))
+    m <- g + geom_boxplot() + coord_flip()
+    } else {
+      print('none')
+    }
+  })
 })
--- project3/ui.R
+++ project3/ui.R
@@ -3,42 +3,51 @@ library(shiny)
 library(tidyverse)
 
 shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel('Project 3'),
-  actionButton('about', 'About'),
-  actionButton('dataex', 'Data Exploration'),
-  actionButton('modeling', 'Modeling'),
-  actionButton('data', 'Data'),
-  # About
-  div(
-    class = 'About Page',
-    navbarPage(
-      "About age")
-  ),
-  #Data Exploration
-  div(
-    class = 'Data Exploration',
-    navbarPage("Data Exploration")
-  ),
-  #Modeling
-  div(
-    class = 'Modeling',
-    navbarPage(
-      "Modeling")
-  ),
-  #Data
-  div(
-    class = 'Data',
-    navbarPage("Data")
-  ),
-  # Sidebar with options for the data set
-    sidebarPanel(
-      
-    ),
-    
-    # Show outputs
-    mainPanel(
-    )
-    ))
- 
+  navbarPage('Golden Project 3',
+    tabPanel('About',
+      titlePanel('About'),
+      sidebarLayout(
+        sidebarPanel(
+
+        ),
+        mainPanel(
+          
+        )
+   )),
+   tabPanel('Data Exploration',
+     titlePanel('Data Exploration'),
+     sidebarLayout(
+       sidebarPanel(
+         radioButtons(inputId = "grade" , 
+                      label = h6(strong("Select Grade Level")), 
+                      choices = c("9th grade",
+                                  "10th grade", 
+                                  "11th grade",
+                                  '12th grade'),
+                      selected = 1)
+       ),
+        mainPanel(
+          plotOutput('height')
+       )
+   )),
+   tabPanel('Modeling',
+     titlePanel('Modeling'),
+     sidebarLayout(
+       sidebarPanel(
+         
+       ),
+      mainPanel(
+        
+      )
+   )),
+   tabPanel('Data',
+     titlePanel('Data'),
+     sidebarLayout(
+       sidebarPanel(
+         
+       ),
+       mainPanel(
+         
+       )
+   ))
+)))
